cdeaaa9707b69c66ab26d93eea9891fe8d4499fb
const-z <->
2017-10-11 02:46:33 +0400

    Test 2 times

--- test/test.js
+++ test/test.js
@@ -6,13 +6,39 @@ const it = require("mocha").it;
 const generate = require("./task-generator").generate;
 
 describe("QueueListener", () => {
-	it("should read messages from queue", async () => {
+
+	const config = require("./config");
+	const QueueListener = require("../");
+	let queueListner = new QueueListener({ db: { url: config.database.mongo.url, collection: config.database.mongo.collection }, delay: 10, limit: 3 });
+
+	it("start 1", async () => {
 		let tasksCount = await generate(14);
 		console.log("task count", tasksCount);
-		const config = require("./config");
-		const QueueListener = require("../");
-		let queueListner = new QueueListener({ db: { url: config.database.mongo.url, collection: config.database.mongo.collection }, delay: 10, limit: 3 });
+		await new Promise((resolve) => {
+			queueListner.on("tasks", (tasks) => {
+				tasks.map((task, i) => {
+					console.log(JSON.stringify(task), "received");
+					setTimeout(() => {
+						tasksCount--;
+						console.log(task.id, "- done. left", tasksCount);
+						task.done();
+						if (tasksCount === 0) {
+							resolve();
+						}
+					}, (i + 1) * 100);
+				});
+			});
+			queueListner.start();
+		});
 
+		queueListner.stop();
+		return;
+	});
+
+	it("start 2", async () => {
+		let tasksCount = await generate(14);
+		console.log("task count", tasksCount);
+		queueListner.removeAllListeners();
 		await new Promise((resolve) => {
 			queueListner.on("tasks", (tasks) => {
 				tasks.map((task, i) => {
