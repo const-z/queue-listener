06e946c7d15ae12a4a9a22a471953b5f898e58db
const-z <->
2017-10-16 23:50:37 +0400

    Test: parallel processing

--- test/test.js
+++ test/test.js
@@ -9,18 +9,19 @@ describe("QueueListener", () => {
 
 	const config = require("./config");
 	const QueueListener = require("../");
-	let queueListner = new QueueListener({ db: { url: config.database.mongo.url, collection: config.database.mongo.collection }, delay: 10, limit: 3 });
+	let queueListner1 = new QueueListener({ db: { url: config.database.mongo.url, collection: config.database.mongo.collection }, delay: 10, limit: 3 });
+	let queueListner2 = new QueueListener({ db: { url: config.database.mongo.url, collection: config.database.mongo.collection }, delay: 10, limit: 3 });
 
 	it("start 1", async () => {
-		let tasksCount = await generate(14);
+		let tasksCount = await generate(34);
 		console.log("task count", tasksCount);
-		await new Promise((resolve) => {
-			queueListner.on("tasks", (tasks) => {
+		await Promise.all([new Promise((resolve) => {
+			queueListner1.on("tasks", (tasks) => {
 				tasks.map((task, i) => {
 					console.log(JSON.stringify(task), "received");
 					setTimeout(() => {
 						tasksCount--;
-						console.log(task.id, "- done. left", tasksCount);
+						console.log(task.id, "queueListner1", "- done. left", tasksCount);
 						task.done();
 						if (tasksCount === 0) {
 							resolve();
@@ -28,18 +29,36 @@ describe("QueueListener", () => {
 					}, (i + 1) * 100);
 				});
 			});
-			queueListner.start();
-		});
+			queueListner1.start();
+		}), new Promise((resolve) => {
+			queueListner2.on("tasks", (tasks) => {
+				tasks.map((task, i) => {
+					console.log(JSON.stringify(task), "received");
+					setTimeout(() => {
+						tasksCount--;
+						console.log(task.id, "queueListner2", "- done. left", tasksCount);
+						task.done();
+						if (tasksCount === 0) {
+							resolve();
+						}
+					}, (i + 1) * 100);
+				});
+			});
+			queueListner2.start();
+		})]);
 
-		return queueListner.stop();		
+		return await Promise.all([
+			queueListner1.stop(),
+			queueListner2.stop()
+		]);
 	});
 
-	it("start 2", async () => {
+	it.skip("start 2", async () => {
 		let tasksCount = await generate(14);
 		console.log("task count", tasksCount);
-		queueListner.removeAllListeners();
+		queueListner1.removeAllListeners();
 		await new Promise((resolve) => {
-			queueListner.on("tasks", (tasks) => {
+			queueListner1.on("tasks", (tasks) => {
 				tasks.map((task, i) => {
 					console.log(JSON.stringify(task), "received");
 					setTimeout(() => {
@@ -52,10 +71,10 @@ describe("QueueListener", () => {
 					}, (i + 1) * 100);
 				});
 			});
-			queueListner.start();
+			queueListner1.start();
 		});
 
-		return queueListner.stop();		
+		return queueListner1.stop();
 	});
 
 });
